5f48ec09eb705f3070ec89ed48fa3f7656012e98
熊熊熊子路 <1711279251@qq.com>
2022-10-01 19:58:58 +0800

    feat: 编写 login 页面样式完成

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,6 +1,13 @@
 <template>
-	<view>
-		
+	<view class="container login-container">
+		<view class="title">登 录</view>
+		<view class="input-box">
+			<u-input class="username input-item" type="text" v-model="userInfo.username" placeholder="请输入手机号"></u-input>
+			<u-input class="password input-item" type="password" v-model="userInfo.password" placeholder="请输入密码"></u-input>
+		</view>
+		<view class="buttons-box">
+			<u-button class="loginBtn" :class="{ active: !isDisable }" :disabled="isDisable">登 录</u-button>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +15,51 @@
 	export default {
 		data() {
 			return {
-				
+				userInfo: {
+					username: "",
+					password: ""
+				},
+				isDisable: true
 			};
+		},
+		watch: {
+			userInfo: {
+				deep: true,
+				handler(value){
+					this.isDisable = !(value.username.length > 0 && value.password.length > 0);
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.login-container{
+	.title{
+		margin-top: 208.33rpx;
+		margin-bottom: 76rpx;
+		text-align: center;
+		font-size: 50rpx;
+		font-weight: 700;
+	}
+	.input-box{
+		.input-item{
+			margin-bottom: 30rpx;
+			border-radius: 16rpx;
+		}
+	}
+	.buttons-box{
+		/* 默认不激活状态下的样子 */
+		.loginBtn{
+			background-color: #ecbdbc;
+			font-size: 25rpx;
+			color: white;
+			border-radius: 16rpx;
+		}
+		/* 激活状态下的样子 */
+		.active{
+			background-color: #ff5850;
+		}
+	}
+}
 </style>
