043439891de01d3f63c4418899d5d81c62d0614c
熊熊熊子路 <1711279251@qq.com>
2022-10-03 02:31:54 +0800

    feat: 实装账户验证函数和用户数据更新模块

--- pages/profile/profile.vue
+++ pages/profile/profile.vue
@@ -5,14 +5,48 @@
 </template>
 
 <script>
+	import { getBaseUrl } from '../../utils/getBaseUrl.js'
+import { verifyIsLogin } from '../../utils/verifyIsLogin.js'
 	export default {
 		data() {
 			return {
-				
+				profile: {}
 			}
 		},
 		methods: {
-			
+			getProfile(){
+				uni.request({
+					url:`${ getBaseUrl() }/profile`,
+					method:'GET',
+					data:{
+						session: uni.getStorageSync('session'),
+						token: uni.getStorageSync('token')
+					},
+					success: (res) => {
+						console.log(res.data)
+						if(res.data.code === 200){
+							this.profile = res.data.data;
+							uni.setStorageSync("userInfo", JSON.stringify({
+								userid: res.data.data["uid"],
+								username: res.data.data["name"],
+								useruname: res.data.data["uname"]
+							}))
+						}
+					},
+					fail: (err) => {
+						console.log(err)
+					}
+				})
+			}
+		},
+		onLoad() {
+			if(!verifyIsLogin()){
+				uni.navigateTo({
+					url:"/pages/login/login"
+				})
+			}else{
+				this.getProfile()
+			}
 		}
 	}
 </script>
