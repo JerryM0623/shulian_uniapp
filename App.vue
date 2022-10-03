d2414a488e4330eb2aecdabb78e304a0fc7cc8a9
熊熊熊子路 <1711279251@qq.com>
2022-10-03 16:38:59 +0800

    feat: 更新全局样式

--- App.vue
+++ App.vue
@@ -15,8 +15,15 @@
 <style lang="scss">
 	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
 	@import "@/uni_modules/uview-ui/index.scss";
+	uni-page-body{
+		height: 100%;
+	}
 	.container{
 		padding: 42rpx 34rpx 0 34rpx;
 		width: 682rpx;
+		height: 100%;
+		font-family: -apple-system, 'Helvetica Neue', Helvetica, 'Nimbus Sans L', Arial, 'Liberation Sans', 'PingFang SC', 'Hiragino Sans GB',
+		  'Source Han Sans CN', 'Source Han Sans SC', 'Microsoft YaHei', 'Wenquanyi Micro Hei', 'WenQuanYi Zen Hei', 'ST Heiti', SimHei,
+		  'WenQuanYi Zen Hei Sharp', sans-serif;
 	}
 </style>
